c4ab77d9af68e4e0f090731820a9a21993e94436
Xavier Nayrac <xavier.nayrac@gmail.com>
2025-11-13 08:41:07 +0100

    Add TXTCOL

---
+++ TXTCOL.asm
@@ -0,0 +1,21 @@
+BasicUpstart2(start)
+
+// Machine Language Routines for the commode 64/128
+//
+// RNDBYT page 538
+//
+// Set text color to green.
+
+.const COLOR = 646 // COLOR = 241 on the 128 — foreground color for text
+
+start:
+  lda COLVAL // Get the color value
+  jsr TXTCOL // and set it
+  rts
+
+// Set text color. Enter with A containing color value.
+TXTCOL:
+  sta COLOR // Set text color
+  rts
+
+COLVAL: .byte 5 // Color green
